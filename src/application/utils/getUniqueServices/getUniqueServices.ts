29eb0e017b33233399d035916ec873013934346b
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-25 23:50:30 +0200

    ADD - get unique service

--- src/application/utils/getUniqueServices/getUniqueServices.ts
+++ src/application/utils/getUniqueServices/getUniqueServices.ts
@@ -1,4 +1,6 @@
-export function getUniqueServices(services: Service[]): Service[] {
+import { FetchedServices } from "@/services/graphql/graphql-types";
+
+export function getUniqueServices(services: FetchedServices): FetchedServices {
   const uniqueServices = new Set<string>();
   return services.filter(service => {
     const serviceId = service.node.id;
