fd887c4feb7265f92926c4bebd1a1bece93de260
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-25 21:15:13 +0200

    ADD - fetch projects server action

--- src/server-actions/railway-services/fetch-projects.ts
+++ src/server-actions/railway-services/fetch-projects.ts
@@ -6,6 +6,26 @@ interface Project {
   name: string,
   id: string
 }
+export interface Service {
+  __typename: string,
+  node: {
+    __typename: string,
+    id: string,
+    name: string
+  }
+}
+
+interface Projectt {
+  __typename: string,
+  node: {
+    __typename: string,
+    id: string,
+    name: string,
+    services: { __typename: string, edges: any[] },
+    plugins: { __typename: string, edges: [] },
+    environments: { __typename: 'ProjectEnvironmentsConnection', edges: any[] }
+  }
+}
 
 const FETCH_PROJECTS_QUERY = gql`
   query {
@@ -53,6 +73,12 @@ export async function fetchAllProjects(): Promise<Project[]> {
       query: FETCH_PROJECTS_QUERY,
     });
 
+    const enviroments = response.data.me.projects.edges.map((project: Projectt) => {
+
+      return project.node.environments
+    })
+
+    console.log("Env: ", enviroments.map((e: any) => e.edges.map((ee: any) => ee.node)))
     return response.data.me.projects.edges.map((project: any) => ({
       id: project.node.id,
       name: project.node.name
