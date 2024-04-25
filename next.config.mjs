fb71cb37cf73af7d3bd008367578aa0070bd3b6f
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-04-25 23:47:47 +0200

    ADD - GQL Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.graphql$/,
+      loader: 'graphql-tag/loader', // Allows importing .graphql files
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
